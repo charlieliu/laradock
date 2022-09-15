b99dbe498fe6464b6a82c43488ee731c67fd8f8b
Charlie LIU <liuchangli0107@gmail.com>
2022-09-15 09:38:12 +0800

    php artisan migrate - mysql insert data

--- database/migrations/2022_09_14_081817_create_quotes_table.php
+++ database/migrations/2022_09_14_081817_create_quotes_table.php
@@ -13,11 +13,32 @@ class CreateQuotesTable extends Migration
      */
     public function up()
     {
-        Schema::create('quotes', function (Blueprint $table) {
+        Schema::create('quotes', function (Blueprint $table)
+        {
             $table->id();
+            $table->string('author', 255);
             $table->string('content', 255);
             $table->timestamps();
         });
+
+        DB::table('quotes')->insert([
+            [
+                'author'        => '愛迪生',
+                'content'       => '失敗為成功之母',
+                'created_at'    => date('Y-m-d H:i:s'),
+                'updated_at'    => date('Y-m-d H:i:s')
+            ],[
+                'author'        => '李奧納多‧達文西',
+                'content'       => '簡潔是最終的精密',
+                'created_at'    => date('Y-m-d H:i:s'),
+                'updated_at'    => date('Y-m-d H:i:s')
+            ],[
+                'author'        => '荷拉斯',
+                'content'       => '好的開始是成功的一半',
+                'created_at'    => date('Y-m-d H:i:s'),
+                'updated_at'    => date('Y-m-d H:i:s')
+            ]
+        ]);
     }
 
     /**
--- database/migrations/2022_09_14_081929_create_posts_table.php
+++ database/migrations/2022_09_14_081929_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('content', 255);
             $table->timestamps();
         });
     }
