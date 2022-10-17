334e43c3cfabb1ccfbbb5e326881c387b52dca68
Charlie LIU <liuchangli0107@gmail.com>
2022-10-17 16:31:31 +0800

    laravel-mix and icon testing
    1. npm install laravel-mix --save-dev
    2. edit webpack.min.js
    3. Run npm run production

--- resources/views/content_list.blade.php
+++ resources/views/content_list.blade.php
@@ -4,11 +4,13 @@
     @if(!empty($columns) && is_array($columns))
         <div class="row">
             @foreach ($columns as $column) <div class="col">{{$column}}</div> @endforeach
+            <div class="col"></div>
         </div>
     @endif
     @foreach ($list as $row)
         <div class="row">
             @foreach ($row as $value) <div class="col">{{$value}}</div> @endforeach
+            <div class="col"><button class="btn"><i class="fa fa-edit"></i></button></div>
         </div>
     @endforeach
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{$title??""}}</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        {{-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        {{-- <style>html {line-height:1.15;}body {margin:0;font-family: 'Nunito', sans-serif;height: 100vh;max-height: calc(100vh - 16px);width: 100vw;}.sidebar {padding:8px;height:100%;float: left;border-radius: 5px;}.sidebar .active, .active .sidebar_1 {color: white;background-color: #04AA6D;}.sidebar a {color: #04AA6D;text-decoration: none;background-color:transparent;}.sidebar_1 {margin: 8px;padding: 8px;width: calc(100% - 16px);border-radius: 5px;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2), 0 2px 10px 0 rgba(0, 0, 0, 0.2);}.sidebar a:hover:not(.active) .sidebar_1 {background-color: #555;color: white;}.container {height: 100%;padding: 16px;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2), 0 2px 10px 0 rgba(0, 0, 0, 0.2);border-radius: 16px;float: left;}.container .row {margin-bottom: 2px;;padding: 8px;box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2), 0 2px 10px 0 rgba(0, 0, 0, 0.2);border-radius: 8px;}.container .row:hover {background-color: wheat;}</style> --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{asset('css/app.min.css')}}">
     </head>
     <body>
         <div class="col-4 col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-            <a role="button" @if ($active=='hello_world') class="active" @endif href="/hello-world"><div class="sidebar_1">Hello World</div></a>
-            <a role="button" @if ($active=='about_us') class="active" @endif href="/about_us"><div class="sidebar_1">About Us</div></a>
-            <a role="button" @if ($active=='inspire') class="active" @endif href="/inspire"><div class="sidebar_1">Inspire</div></a>
+            <a role="button" @if ($active=='hello_world') class="active" @endif href="/hello-world">
+                <div class="sidebar_1"><i class="fa fa-home"></i> Hello World</div>
+            </a>
+            <a role="button" @if ($active=='about_us') class="active" @endif href="/about_us">
+                <div class="sidebar_1"><i class="fa fa-bars"></i> About Us</div>
+            </a>
+            <a role="button" @if ($active=='inspire') class="active" @endif href="/inspire">
+                <div class="sidebar_1"><i class="fa fa-bars"></i> Inspire</div>
+            </a>
         </div>
         <div class="col-8 col-md-9 col-lg-10 bg-light container">
             @yield('content')
